e442277b2b6c2233a8c91a8a1cada1e86b9657e7
Superbia91 <fierr5231@gmail.com>
2026-02-03 21:02:09 +0300

    Add RevolutionCasePool

---
+++ src/main/java/net/superbia/caseopener/loot/pools/RevolutionCasePool.java
@@ -0,0 +1,9 @@
+package net.superbia.caseopener.loot.pools;
+
+import net.superbia.caseopener.loot.DropEntry;
+
+import java.util.List;
+
+public class ShadowDaggersPool {
+    public static final List<DropEntry> 
+}
